b747978a7a5a1227bdfb6a639be7e9b3349e7498
Berk Ozbalci <berkozbalci@gmail.com>
2018-04-29 04:18:20 +0300

    wrote some comments before going to bed

--- the3/the3.c
+++ the3/the3.c
@@ -1,3 +1,15 @@
+/*
+ * CENG336, Intro. to Embedded Systems Development
+ * Spring 2018
+ * Take Home Exam III, rev. 1.1, deadline: 2018-04-30 23:55
+ *
+ * Group 09
+ * Yağmur Oymak 2171783
+ * Berk Özbalcı 2171791
+ *
+ * TODO: Clear out TODOs!
+ */
+
 #pragma config OSC = HSPLL, FCMEN = OFF, IESO = OFF
 #pragma config PWRT = OFF, BOREN = OFF, BORV = 3
 #pragma config WDT = OFF, WDTPS = 32768
@@ -20,8 +32,12 @@
 #include <xc.h>
 #include <stdint.h>
 
-#define T0_5MS_INITIAL 61
+#define T0_5MS_INITIAL 61 // Required to produce a delay of 5ms
+#define T1_50MS_INITIAL 0xBE3 // Required to produce a delay of 50ms
 
+// This enumerated type holds the current state of the program. Each of these
+// states are handled separately. Each state has its own rules for transitioning
+// into different states.
 typedef enum
 {
     PS_INITIAL,
@@ -35,22 +51,59 @@ typedef enum
     PS_FAILURE
 } ProgramState;
 
+// TODO The program state should probably not be volatile? It's not being used in ISRs
 volatile ProgramState state;
 
-volatile uint8_t t0_times, t1_times, adc_counter;
-volatile uint8_t promise_change_digit, promise_pin_confirmed, promise_can_promise;
+volatile uint8_t t0_times; // Number of Timer0 interrupts handled
+volatile uint8_t t1_times; // Number of Timer1 interrupts handled
+volatile uint8_t adc_counter; // Seperate counter for ADC conversion in Timer0
+
+// The Promise-Commit pattern is used in this code. Named by the authors, the
+// "promise" is a flag that is set by an external subroutine and then "committed"
+// in the main loop of the program. Once committed, the promise is said to be
+// invalidated (set to zero). It is possible to extend this mechanism to multiple
+// by using non-binary values, but in this program we chose not to.
+volatile uint8_t promise_change_digit; // Promise given by RB6 event
+volatile uint8_t promise_pin_confirmed; // Promise given by RB7 event
+
+// promise_can_promise is a promise given by the main loop to the PORTB button
+// handlers. When committed, promise_change_digit and promise_pin_confirmed can
+// be dealt by their respective handlers.
+volatile uint8_t promise_can_promise;
+
+// These variables are set when either of the respective buttons are pressed.
+// They are checked against the current value of the buttons to verify that
+// they have been released.
 volatile uint8_t rb6_pressed, rb7_pressed;
 
+// ad_result is constantly updated on ADC conversion. It is the non-normalized
+// value of the potentiometer, ranging between 0 and 1023.
 volatile int16_t ad_result;
+
+// pot_last is updated just before new user input by the potentiometer is
+// required. It captures the value of ad_result at that instant, and is
+// checked against the subsequent values of ad_result to determine if the
+// user had provided new inputs.
 int16_t pot_last;
 
+// should_blink is set when no input has been given for the current digit of
+// the pin, that is exactly when the pound symbol shall blink on the LCD.
+// pound is a constantly alternating variable. When cleared, the LCD displays
+// a whitespace character, and when set, the LCD displays a pound symbol.
 uint8_t should_blink, pound;
 
-uint8_t pin[4];
-uint8_t input_pin[4];
+// pin holds the PIN that was set by the user in PS_PINSETTING. input_pin holds
+// the PINs that are entered by the user in the test stage, namely PS_TEST and
+// PS_TARPIT.
+uint8_t pin[4], input_pin[4];
 
-int8_t countdown = 120, attempts, countdown_snapshot;
+int8_t countdown = 120; // Seconds left before program restart
+int8_t attempts; // Number of attempts left
+int8_t countdown_snapshot; // An instantaneous value of countdown
 
+// This function compares the current potentiometer value to a previous value.
+// If the difference is larger than the threshold value of 50, it returns 1.
+// Otherwise, it returns 0.
 uint8_t pot_updated(void)
 {
     int16_t diff = pot_last - ad_result;
@@ -58,9 +111,12 @@ uint8_t pot_updated(void)
     return (diff >= 50) || (diff <= -50);
 }
 
-/* [S]leep [F]or [S]even [S]egment [D]isplay [U]pdate */
+// [S]leep [F]or [S]even [S]egment [D]isplay [U]pdate. This horribly named
+// function produces a delay that is sufficiently long to display outputs
+// on the seven segment display with a tolerable level of flickering.
 void sfssdu(void)
-{
+{ 
+    // TODO Maybe this is better implemented as a preprocessor macro.
     uint16_t i = 1023;
 
     while (i)
@@ -69,16 +125,22 @@ void sfssdu(void)
     }
 }
 
+// zeg_clear clears the seven segment display. If you squint hard enough,
+// the letter z looks like the digit 7, which is supposed to remind you that
+// this function operates on the seven segment display. The following functions
+// are named similarly.
 void zeg_clear(void)
 {
     PORTJ = 0;
     PORTH = 8;
+    
     do
     {
         ;
     } while (PORTH >>= 1);
 }
 
+// zeg_dashes displays four dashes on the seven segment display.
 void zeg_dashes(void)
 {
     PORTJ = 1 << 6;
@@ -90,6 +152,8 @@ void zeg_dashes(void)
     } while (PORTH >>= 1);
 }
 
+// get_zeg_repr takes a digit as input and returns the corresponding segment
+// display configuration. This value is then written to PORTJ.
 int8_t get_zeg_repr(int8_t digit)
 {
     switch (digit)
@@ -117,12 +181,16 @@ int8_t get_zeg_repr(int8_t digit)
     }
 }
 
+// zeg_number displays a positive number up to 127 on the seven segment display.
+// The number is zero-padded to the length of the display (4).
 void zeg_number(int8_t num)
 {
+    // The most significant digit is always zero in our specifications.
     PORTH = 1;
     PORTJ = get_zeg_repr(0);
     sfssdu();
 
+    // The third digit is either 1 or 0, depending on how large the number is.
     PORTH = 2;
     if (num >= 100)
     {
@@ -145,8 +213,60 @@ void zeg_number(int8_t num)
     sfssdu();
 }
 
+// delay_3s produces a three second delay. This function was implemented in
+// PIC assembly on a previous assignment and was shamelessly copied verbatim
+// into this one.
+void delay_3s(void)
+{
+    uint8_t t1 = 0, t2 = 0, t3 = 0;
+#asm
+    MOVLW 0x99
+    MOVWF delay_3s@t3
+_loop3:
+    MOVLW 0xFF
+    MOVWF delay_3s@t2
+_loop2:
+    MOVLW 0xFF
+    MOVWF delay_3s@t1
+_loop1:
+    DECFSZ delay_3s@t1
+    BRA _loop1
+    DECFSZ delay_3s@t2
+    BRA _loop2
+    DECFSZ delay_3s@t3
+    BRA _loop3
+#endasm
+}
+
+// normalize_ad returns a normalized value of the ADC output. According to the
+// specifications, 0-99 maps to 0, 100-199 maps to 1, and so on, until 999-1023
+// which maps to 9.
+int8_t normalize_ad(int16_t ad)
+{
+    int8_t result;
+
+    if (ad < 1000)
+    {
+        result = ad / 100;
+        return result;
+    }
+
+    return 9;
+}
+
+// get_lcd_repr takes a digit and returns a character suitable for printing onto
+// the LCD screen.
+char get_lcd_repr(int8_t val)
+{
+    // TODO Maybe this is better implemented as a preprocessor macro.
+    
+    return '0' + val;
+}
+
 void init(void)
 {
+    // TODO Comment and organize this mess
+    
     TRISA = 0b00111111;
     PORTA = 0;
     // Configure RB7 and RB6 as inputs, the rest of the PORTB is unused.
@@ -196,92 +316,70 @@ void init(void)
     ei();
 }
 
-void delay_3s(void)
-{
-    uint8_t t1 = 0, t2 = 0, t3 = 0;
-#asm
-    MOVLW 0x99
-    MOVWF delay_3s@t3
-_loop3:
-    MOVLW 0xFF
-    MOVWF delay_3s@t2
-_loop2:
-    MOVLW 0xFF
-    MOVWF delay_3s@t1
-_loop1:
-    DECFSZ delay_3s@t1
-    BRA _loop1
-    DECFSZ delay_3s@t2
-    BRA _loop2
-    DECFSZ delay_3s@t3
-    BRA _loop3
-#endasm
-}
-
-int8_t normalize_ad(int16_t ad)
-{
-    int8_t result;
-
-    if (ad < 1000)
-    {
-        result = ad / 100;
-        return result;
-    }
-
-    return 9;
-}
-
-char get_lcd_repr(int8_t val)
-{
-    return '0' + val;
-}
-
+// isr is the interrupt service routine.
 void interrupt isr(void)
 {
     if (TMR0IF)
     {
-        TMR0L = T0_5MS_INITIAL;
-        TMR0IF = 0;
+        // Timer0 interrupt.
+        
+        // Increment t0_times. As the desired number of Timer0 interrupts are different
+        // in different states of the program, it is within responsibility of the
+        // main loop to check this counter against a value and reset it afterwards.
         t0_times++;
-
-        /* Count for ADC only if we're in the stage of setting the PIN,
-           i.e. analog inputs are required. */
+        
+        // Increment adc_counter. On every 20th increment, a period of 100 ms passes
+        // and the counter is reset. Then, the GO bit of ADCON0 is set to trigger
+        // ADC conversion.
         if (++adc_counter == 20)
         {
-            adc_counter = 0;
             ADCON0bits.GO = 1;
+            adc_counter = 0;
         }
-
+        
+        TMR0L = T0_5MS_INITIAL;
+        TMR0IF = 0;
     }
     else if (TMR1IF)
     {
-        TMR1IF = 0;
-        TMR1 = 0xBE3;
+        // Timer1 interrupt.
+        
+        // Increment t1_times. On every 20th increment, a period of 1 s passes and
+        // the counter is reset, and countdown is decremented.
         if (++t1_times == 20) 
         {
-            t1_times = 0;
             countdown--;
+            t1_times = 0;
         }
+        
+        TMR1 = T1_50MS_INITIAL;
+        TMR1IF = 0;
     }
     else if (ADIF)
     {
+        // ADC interrupt.
+        
+        // Capture the output of the ADC module into ad_result and dispatch.
         ad_result = ADRES;
+        
         ADIF = 0;
     }
     else if (RBIF)
     {
-        /* This unorthodox method of reading PORTB is probably required to
-           force a read on all of the bits in PORTB, therefore ending the
-           mismatch condition and allow the RBIF bit to be cleared. */
+        // This unorthodox method of reading PORTB is probably required to
+        // force a read on all of the bits in PORTB, therefore ending the
+        // mismatch condition and allow the RBIF bit to be cleared.
         if (PORTB)
         {
 #asm
             nop
 #endasm
         }
-
-        RBIF = 0;
-
+        
+        // If promise_can_promise is not given, then the release of RB6 or
+        // RB7 should not yield a promise_change_digit or promise_pin_confirmed.
+        // In this case we clear rb6_pressed and rb7_pressed, effectively discarding
+        // button presses prior to the issuance of promise_can_promise.
         if (rb6_pressed && !promise_can_promise)
         {
             rb6_pressed = 0;
@@ -291,7 +389,9 @@ void interrupt isr(void)
         {
             rb7_pressed = 0;
         }
-
+        
+        // When promise_can_promise is given, the following if-block, and its subsequent,
+        // will capture a press and release and issue their respective promises.
         if (!PORTBbits.RB6)
         {
             rb6_pressed = 1;
@@ -311,13 +411,15 @@ void interrupt isr(void)
             promise_pin_confirmed = 1;
             rb7_pressed = 0;
         }
+        
+        RBIF = 0;
     }
 }
 
-
-
 void main(void)
 {
+    // TODO Comment and organize this
+    
     uint8_t digits_entered;
     uint8_t pin_digit;
     uint8_t count = 0;
