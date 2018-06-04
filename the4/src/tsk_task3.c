21072bf75c7b4a578337019315d14c395b570fcf
Berk Ozbalci <berkozbalci@gmail.com>
2018-06-04 13:28:25 +0300

    dönemin son commiti?

--- the4/src/tsk_task3.c
+++ the4/src/tsk_task3.c
@@ -1,3 +1,16 @@
+// This is the cleanest fucking code any student could come up with.
+// It should be included as a complete listing in Steve McConnell's
+// Code Complete 2, it should be taught in the SE course Ceng350
+// as a prime example of readable and understandable code.
+//
+// Yet it doesn't work.
+//
+// It has a very simple task of updating the robot state and updating
+// the command object. The frequency of this task is every 10 commands,
+// because any rotation or forward motion will take 10 commands. It uses
+// mutual exclusion to prevent other tasks from fiddling with cin and cout
+// while doing its job. Perfect
+
 #include "common.h"
 #include "path.h"
 #include "cmd.h"
@@ -19,8 +32,6 @@ TASK(TASK3)
         }
 
         robotstate_update(&rs, &cin_copy);
-
-        // TODO Implement motion planning
 
         if (GetResource(1) == E_OK)
         {
